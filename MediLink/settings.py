0c5e2961dc361871b937f9a53c9df915f7bf39dd
Jay <w742794749@gmail.com>
2023-11-06 21:22:36 -0500

    static

--- MediLink/settings.py
+++ MediLink/settings.py
@@ -156,18 +156,18 @@ EMAIL_USE_SSL = False
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-AWS_ACCESS_KEY_ID = 'AKIASCZJFLUHP65L3M75'
-AWS_SECRET_ACCESS_KEY = 'bcOD9AaxNpLEve3KQmpDG/0RsAhiMR+1B4koNBRS'
-AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-west-2-143430868238'
-AWS_S3_REGION_NAME = 'us-west-2'  # Replace with your bucket region
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_FILE_OVERWRITE = False
-AWS_S3_SECURE_URLS = True
+# AWS_ACCESS_KEY_ID = 'AKIASCZJFLUHP65L3M75'
+# AWS_SECRET_ACCESS_KEY = 'bcOD9AaxNpLEve3KQmpDG/0RsAhiMR+1B4koNBRS'
+# AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-west-2-143430868238'
+# AWS_S3_REGION_NAME = 'us-west-2'  # Replace with your bucket region
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_SECURE_URLS = True
 
-# Use Amazon S3 for storing media files.
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# # Use Amazon S3 for storing media files.
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# Set the S3 URL for media files.
-MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+# # Set the S3 URL for media files.
+# MEDIA_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
